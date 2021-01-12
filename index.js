85bc9f9707eaa32e2bf785b6a50b7ee542ccef66
Nhat Nguyen <nhat.nguyen1125@gmail.com>
2021-01-12 18:07:52 +0700

    a

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const cors = require("cors");
 const app = express();
 const PORT = 9333;
 
+app.use(express.json());
 app.get("/", (req, res) => res.send("payment test demo"));
 app.all("/approve", (req, res) => {
   console.log("request received");
-  console.log(req.body);
+  console.log(req.body.test);
   res.json({ status: "ok" });
 });
 
