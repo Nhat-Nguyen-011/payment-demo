3b5b52cdf7e24e3effa91679bd7f9d3c58e26061
Nhat Nguyen <nhat.nguyen1125@gmail.com>
2021-01-12 16:00:51 +0700

    a

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const PORT = 9333;
 
 app.get("/", (req, res) => res.send("payment test demo"));
 app.post("/approve", (req, res) => {
   console.log(req.body);
   res.json({ status: "ok" });
+});
+
+app.listen(PORT, () => {
+  console.log(`App is listen on port ${PORT}`);
 });
